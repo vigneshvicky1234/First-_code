8e0b589fcffbbf98b72c6fa985909beca0c1e55d
vigneshvicky1234 <127626606+vigneshvicky1234@users.noreply.github.com>
2023-03-12 22:29:43 +0530

    Add files via upload

---
+++ Prime Number.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(" Enter your Number");
+int a = int.Parse(Console.ReadLine());
+int sum = 0;
+for (int i = 1; i <= a; i++)
+{
+    if (a % i == 0)
+    {
+        sum++;
+    }
+}
+if(sum ==2)
+{
+    Console.WriteLine(a + " is prime number ");
+}
+else
+{
+    Console.WriteLine(a + " it is not prime ");
+}
